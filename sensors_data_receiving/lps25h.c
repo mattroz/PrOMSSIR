6a31de26dc828094e02ef6801c50379f5a8953a4
mattroz <mattroz@github.com>
2017-01-20 09:35:26 +0000

    i2c device connection added

--- sensors_data_receiving/lps25h.c
+++ sensors_data_receiving/lps25h.c
@@ -5,8 +5,8 @@
 #include <linux/i2c-dev.h>
 
 /*	main device defining registers */
-#define DEV_ADDR			0x5C
-#define DEV_ID				0xBD
+#define DEV_ADDR		0x5C
+#define DEV_ID			0xBD
 #define WHO_AM_I_ADDR	0x0F
 
 /*	control registers for enabling I2C interface, single shot measurment etc	*/
@@ -20,8 +20,20 @@
 #define TEMP_OUT_L 	0x2B
 #define TEMP_OUT_H	0x2C
 
+/*	defines for I2C 	*/
+#define I2C_PATH	"/dev/i2c-1"
 
 int main()
 {
+	/*	open i2c bus for communication	*/
+	int i2c_fd = 0;
+	if(i2c_fd = open(I2C_PATH, 0_RDWR) < 0)
+	{
+		perror("I2C device");
+		exit(EXIT_FAILURE);
+	}
+
+
+
 	return 0;
 }
