04052cc54c314513da610e0be151c6939ad0752e
mattroz <mattroz@github.com>
2017-01-20 09:25:58 +0000

    Indents fixes

--- sensors_data_receiving/lps25h.c
+++ sensors_data_receiving/lps25h.c
@@ -5,16 +5,16 @@
 #include <linux/i2c-dev.h>
 
 /*	main device defining registers */
-#define DEV_ADDR 			0x5C
-#define DEV_ID 			0xBD
-#define WHO_AM_I_ADDR 	0x0F
+#define DEV_ADDR			0x5C
+#define DEV_ID				0xBD
+#define WHO_AM_I_ADDR	0x0F
 
 /*	control registers for enabling I2C interface, single shot measurment etc	*/
-#define CTRL_REG1 0x20
-#define CTRL_REG2 0x21
+#define CTRL_REG1	0x20
+#define CTRL_REG2	0x21
 
 /*	registers containing pressure	and temperature measurments	*/
-#define PRESS_OUT_XL 0x28
+#define PRESS_OUT_XL	0x28
 #define PRESS_OUT_L 	0x29
 #define PRESS_OUT_H 	0x2A
 #define TEMP_OUT_L 	0x2B
